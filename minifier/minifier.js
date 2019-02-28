9d98a2fec036a5b72f149cb4357428a7add152eb
Márk Balyi <mbalyi@Mark-MacBook-Air.local>
2019-02-28 21:10:08 +0100

    Creating minifier and default min js and css.

---
+++ minifier/minifier.js
@@ -0,0 +1,34 @@
+const minify = require('./node_modules/@node-minify/core');
+const sqwish = require('./node_modules/@node-minify/sqwish');
+const gcc = require('./node_modules/@node-minify/google-closure-compiler');
+
+console.info('Minifier of JS files have been started.');
+minify({
+    compressor: gcc,
+    input: './../src/js/**/*.js',
+    output: './../chao.min.js',
+    type: 'js',
+    sync: true,
+    callback: function(err, min) {
+        if (err === null) {
+            console.info(`JS minifier finished with success result.`);
+        } else {
+            console.error(err);
+        }
+    }
+});
+
+console.info('Minifier of CSS files have been started.');
+minify({
+    compressor: sqwish,
+    input: './../src/css/**/*.css',
+    output: './../chao.min.css',
+    type: 'css',
+    callback: function(err, min) {
+        if (err === null) {
+            console.info(`CSS minifier finished with success result`);
+        } else {
+            console.error(err);
+        }
+    }
+});
---
+++ src/js/dropdown.component.js
@@ -0,0 +1,6 @@
+/**
+ * CHAO.JS: Dropdown component
+ */
+"use strict";
+
+var ChaoDropdown = function(options = {}) {}
