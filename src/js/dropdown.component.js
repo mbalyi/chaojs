3fa1d618750298692b84cbcc077f94ef25e25f4c
Márk Balyi <mbalyi@Mark-MacBook-Air.local>
2019-03-02 15:02:59 +0100

    Creating working basic dropdown logic and a sample page.

--- src/js/dropdown.component.js
+++ src/js/dropdown.component.js
@@ -11,28 +11,28 @@ var ChaoDropdown = function(options = {}) {
     // Render the default html DOM structure
     this.render = function() {
         let dropdown = `
-‹div›
-    ‹div class="chao-dropdown ${this._options.customClass}" id="chao-${this.$target.att('id')}"›
-        ‹div class="chao-dropdown-body"›
-            ‹div class="chao-select"›${this._options.defaultTitle}‹/div›
-            ‹div class="chao-arrow"›‹/div›
-        ‹/div›
-        ‹div class="chao-dropdown-list"›‹/div›
-        ‹input id="${this.$target.attr('id')}" data-role="dropdown"›
-    ‹/div›
-‹/div›
+<div>
+    <div class="chao-dropdown ${this._options.customClass}" id="chao-${this.$target.attr('id')}">
+        <div class="chao-dropdown-body">
+            <div class="chao-select">${this._options.defaultTitle}</div>
+            <div class="chao-arrow"></div>
+        </div>
+        <div class="chao-dropdown-list"></div>
+        <input id="${this.$target.attr('id')}" data-role="dropdown">
+    </div>
+</div>
         `
-        this.$target.replaceWith(dropdown.html());
+        this.$target.replaceWith($.parseHTML(dropdown));
     }
 
     /** Common reload logic */
     this.reload = function(data) {
         if (Array.isArray(data) === true) {
             // Use available list.
-            _reload(data);
+            this._reload(data);
         } else if (typeof(data) === 'object') {
             // Request list via HTTP
-            //_request(data);
+            //this._request(data);
             console.warn('Current dynamic list loading is NOT SUPPORTED via HTTP request!');
         } else {
             throw new Error('Not supported payload. Payload must be array or object!');
@@ -41,7 +41,8 @@ var ChaoDropdown = function(options = {}) {
 
     /** Currently NOT supported! */
     /** Get dropdown data via HTTP request using got payload */
-    let _request = function(payload) {
+    this._request = function(payload) {
+        let self = this;
         $.ajax({
             url: payload.url,
             type: payload.requestType ? payload.requestType : 'GET',
@@ -49,7 +50,7 @@ var ChaoDropdown = function(options = {}) {
             data: payload.data ? payload.data : undefined,
             succes: function(data) {
                 if (Array.isArray(data) === true) {
-                    _reload(data);
+                    self._reload(data);
                 } else {
                     console.error('Received data is not a list!');
                     throw new Error('Received data is not a list!');
@@ -62,6 +63,17 @@ var ChaoDropdown = function(options = {}) {
         });
     }
 
+    /** Reload the data and rerender the list items */
+    this._reload = function(data = []) {
+        let list = $('<div/>');
+
+        data.forEach(item => {
+            list.append(`<div class="chao-item" data-chao-id="${item.id}">${item.text}</div>`);
+        });
+
+        $(`#chao-${this.$target.attr('id')} .chao-dropdown-list`).empty().append(list.html());
+    }
+
     /** Init */
     this.init = function() {
         this.render();
@@ -71,4 +83,7 @@ var ChaoDropdown = function(options = {}) {
             this.reload(this._data);
         }
     }
+
+    this.init();
+    return this;
 }
