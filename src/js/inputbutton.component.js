1d97f4b2ed7b63b3975a2288caf687fa36bb4c54
Mark Balyi <balyi.mark@hotmail.com>
2019-05-04 16:21:16 +0200

    ChaoInput component functionality.

--- src/js/inputbutton.component.js
+++ src/js/inputbutton.component.js
@@ -7,31 +7,28 @@ var ChaoInputButton = function(options = {}) {
     this._options = options;
     this.$target = this._options.target;
     this.$element = null;
+    this._input = null;
+    this._btn = null;
 
-    this.init = function() {
+    this.renderInput = function() {
+        this._input = $('input', this.$element).chaoInput(this._options);
     }
 
-    this.handleBindings = function() {
-        let self = this;
-        $(this.$element).unbind();
-        
-        $(this.$element).on('click', e => {
-            if (!self._options.disabled && self._options.callback && self._options.callback.onClick) {
-                self._options.callback.onClick(e);
-            }
-        });
+    this.renderBtn = function() {
+        if (this._options.type === ChaoButtonType.iconWithoutBorderBtn) {
+            this._options.type = ChaoButtonType.iconBtn;
+        }
+        this._btn = $('button', this.$element).chaoButton(this._options);
+    }
 
-        $(this.$element).on('mouseenter', e => {
-            if (self._options.callback && self._options.callback.onMouseEnter) {
-                self._options.callback.onMouseEnter(e);
-            }
-        });
-
-        $(this.$element).on('mousedown', e => {
-            if (!self._options.disabled && self._options.callback && self._options.callback.onMouseDown) {
-                self._options.callback.onMouseDown(e);
-            }
-        });
+    this.init = function() {
+        let _id = this._options.customId !== undefined && this._options.customId !== null ? this._options.customId : this.$target.attr('id');
+        let _inputBtn = `<div class="chao-input-btn ${this._options.customClass ? this._options.customClass : ''}" ${_id !== undefined && _id !== null ? `id="chao-${_id}"` : ''}><input><button></button></div>`;
+        this.$element = $(_inputBtn);
+        this.$target.replaceWith(this.$element);
+        this.renderInput();
+        this.renderBtn();
+        this.$element.data('chaoInputButton', this);
     }
 
     this.enable = ChaoFormService.getInstance().enable;
