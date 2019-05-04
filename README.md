06aea8e64a81bf94ab359ab031865c186b9a9728
Mark Balyi <balyi.mark@hotmail.com>
2019-05-04 19:59:31 +0200

    Adding test scenarios for InputButton component. Update README. Minify src.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Chao.JS is an open-source library with few common HTML component for JavaScript.
 
 Most of the solutions is similar to kendoUI, but with many simplification and with unique CSS design.
 
-**Version**: 0.8.5.
+**Version**: 0.9.5.
 
 ## Installation and import
 
@@ -49,6 +49,7 @@ More examples are available under [examples](https://github.com/mbalyi/chaojs/tr
 
 ## Available components
 - Input
+- InputButton
 - Validator
 - Dropdown [WIP]
 - Button
---
+++ tests/spec/specs/InputButtonComponentSpec.js
@@ -0,0 +1,242 @@
+var setupHtmlContext = function() {
+  let _field1 = `<div id="chaoInputButton" value="Default Text Field" title="First input button"></div>`;
+  $('body').empty().append($.parseHTML(_field1));
+  let _field2 = `<div class="empty"></div>`;
+  $('body').append($.parseHTML(_field2));
+}
+
+describe("InputButtonComponent", function() {
+
+  beforeEach(function() {
+    setupHtmlContext();
+  });
+
+  it("[Chao InputButton Component] field is rendered with jQuery and default configuration.", function() {
+    // console.log(`[Chao InputButton Component] field is rendered with jQuery and default configuration.`);
+    expect($('#chaoInputButton')).toExist();
+    $('#chaoInputButton').chaoInputButton();
+    expect($('#chaoInputButton')).not.toExist();
+    let $inputBtn = $('.chao-input-btn');
+    let $input = $('.chao-input', $inputBtn);
+    let $btn = $('.chao-btn', $inputBtn);
+
+    expect($inputBtn).toExist();
+    expect($inputBtn).toHaveData('chaoInputButton');
+
+    expect($input).toExist();
+    expect($input).not.toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).toHaveClass('chao-text');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+    expect($input).not.toHaveAttr('required');
+    expect($input).toHaveAttr('title');
+    expect($input.val()).toEqual('Default Text Field');
+    expect($input.attr('title')).toEqual('First input button');
+
+    expect($btn).toExist();
+    expect($btn).not.toBeDisabled();
+    expect($btn).toHaveClass('chao-btn');
+    expect($btn).toHaveClass('chao-button');
+    expect($btn).toHaveClass('chao-btn-info');
+    expect($btn).not.toHaveClass('chao-disabled');
+  });
+
+  it("[Chao InputButton Component] default renderer parameters.", function() {
+    // console.log(`[Chao InputButton Component] default renderer parameters.`);
+    expect($('.empty')).toExist();
+    let _inputBtn = $('.empty').chaoInputButton();
+    expect($('.empty')).not.toExist();
+    let $inputBtn = _inputBtn.$element;
+    let $input = $('.chao-input', $inputBtn);
+    let $btn = $('.chao-btn', $inputBtn);
+
+    expect($inputBtn).toExist();
+    expect($inputBtn).toHaveData('chaoInputButton');
+    
+    expect($input).toExist();
+    expect($input).not.toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).toHaveClass('chao-text');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+    expect($input).not.toHaveAttr('required');
+    expect($input).not.toHaveAttr('title');
+    expect($input.val()).toEqual('');
+
+    expect($btn).toExist();
+    expect($btn).not.toBeDisabled();
+    expect($btn).toHaveClass('chao-btn');
+    expect($btn).toHaveClass('chao-button');
+    expect($btn).toHaveClass('chao-btn-info');
+    expect($btn).not.toHaveClass('chao-disabled');
+  });
+
+  it("[Chao InputButton Component] field is rendered with configuration.", function() {
+    // console.log(`[Chao InputButton Component] field is rendered with configuration.`);
+    expect($('#chaoInputButton')).toExist();
+    $('#chaoInputButton').chaoInputButton({
+      readonly: true,
+      buttonType: 'button',
+      severity: 'success',
+      value: 'Message was sent to client!',
+      buttonTitle: 'Send',
+      required: true,
+      inputType: 'number',
+      buttonType: 'buttonWithIcon',
+      icon: 'fa fa-alert',
+      severity: 'error',
+      buttonTitle: 'Alert'
+    });
+    expect($('#chaoInputButton')).not.toExist();
+    let $inputBtn = $('#chao-chaoInputButton');
+    let $input = $('.chao-input', $inputBtn);
+    let $btn = $('.chao-btn', $inputBtn);
+
+    expect($input).not.toBeDisabled();
+    expect($input).toHaveAttr('readonly');
+    expect($input).toHaveAttr('required');
+    expect($input).toHaveClass('chao-number');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).toHaveClass('chao-readonly');
+    expect($input).toHaveData('chaoInput');
+
+    expect($btn).toBeDisabled();
+    expect($btn).toExist();
+    expect($btn).toHaveClass('chao-buttonWithIcon');
+    expect($btn).toHaveClass('chao-btn-error');
+    expect($('.chao-btn-icon', $btn)).toExist();
+    expect($('.chao-btn-icon', $btn)).toHaveClass('fa-alert');
+    expect($('.chao-btn-title', $btn)).toExist();
+    expect($('.chao-btn-title', $btn).html()).toEqual('Alert');
+    expect($btn).not.toHaveAttr('readonly');
+    expect($btn).not.toHaveAttr('required');
+    expect($btn).toHaveClass('chao-disabled');
+    expect($btn).not.toHaveClass('chao-readonly');
+    expect($btn).toHaveData('chaoButton');
+  });
+
+  it("[Chao InputButton Component] init result and data on DOM are same.", function() {
+    // console.log(`[Chao InputButton Component] init result and data on DOM are same.`);
+    let _data = $('#chaoInputButton').chaoInputButton();
+    let _dataFromDOM = $('#chao-chaoInputButton').data('chaoInputButton');
+    expect(_data).toEqual(_dataFromDOM);
+  });
+
+  it("[Chao InputButton Component] enable/readonly function.", function() {
+    // console.log(`[Chao InputButton Component] enable/readonly function.`);
+    $('#chaoInputButton').chaoInputButton();
+    let $inputBtn = $('.chao-input-btn');
+    let $input = $('.chao-input', $inputBtn);
+    let $btn = $('.chao-btn', $inputBtn);
+    let _inputBtn = $inputBtn.data('chaoInputButton');
+
+    expect($input).not.toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+
+    expect($btn).not.toBeDisabled();
+    expect($btn).not.toHaveClass('chao-disabled');
+
+    _inputBtn.enable(false);
+
+    expect($input).toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+
+    expect($btn).toBeDisabled();
+    expect($btn).toHaveClass('chao-disabled');
+
+    _inputBtn.enable();
+
+    expect($input).not.toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+
+    expect($btn).not.toBeDisabled();
+    expect($btn).not.toHaveClass('chao-disabled');
+
+    _inputBtn.readonly();
+
+    expect($input).not.toBeDisabled();
+    expect($input).toHaveAttr('readonly');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).toHaveClass('chao-readonly');
+
+    expect($btn).toBeDisabled();
+    expect($btn).toHaveClass('chao-disabled');
+
+    _inputBtn.enable();
+
+    expect($input).not.toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+
+    expect($btn).not.toBeDisabled();
+    expect($btn).not.toHaveClass('chao-disabled');
+  });
+
+  it("[Chao InputButton Component] enable from config.", function() {
+    // console.log(`[Chao InputButton Component] enable from config.`);
+    $('#chaoInputButton').chaoInputButton({
+      disabled: true
+    });
+    let $inputBtn = $('.chao-input-btn');
+    let $input = $('.chao-input', $inputBtn);
+    let $btn = $('.chao-btn', $inputBtn);
+    let _inputBtn = $inputBtn.data('chaoInputButton');
+
+    expect($input).toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+
+    expect($btn).toBeDisabled();
+    expect($btn).toHaveClass('chao-disabled');
+
+    _inputBtn.enable();
+
+    expect($input).not.toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+
+    expect($btn).not.toBeDisabled();
+    expect($btn).not.toHaveClass('chao-disabled');
+  });
+
+  it("[Chao InputButton Component] readonly from config.", function() {
+    // console.log(`[Chao InputButton Component] readonly from config.`);
+    $('#chaoInputButton').chaoInputButton({
+      readonly: true
+    });
+    let $inputBtn = $('.chao-input-btn');
+    let $input = $('.chao-input', $inputBtn);
+    let $btn = $('.chao-btn', $inputBtn);
+    let _inputBtn = $inputBtn.data('chaoInputButton');
+
+    expect($input).not.toBeDisabled();
+    expect($input).toHaveAttr('readonly');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).toHaveClass('chao-readonly');
+
+    expect($btn).toBeDisabled();
+    expect($btn).toHaveClass('chao-disabled');
+
+    _inputBtn.enable();
+
+    expect($input).not.toBeDisabled();
+    expect($input).not.toHaveAttr('readonly');
+    expect($input).not.toHaveClass('chao-disabled');
+    expect($input).not.toHaveClass('chao-readonly');
+
+    expect($btn).not.toBeDisabled();
+    expect($btn).not.toHaveClass('chao-disabled');
+  });
+
+  /** TODO: Need more tests for code and function coverage!!! */
+});
